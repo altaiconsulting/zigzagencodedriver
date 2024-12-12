dc80dbafe2a64504918a28ee6b0c7c2ae8043fa8
altaiconsulting <altaiconsultinginc@gmail.com>
2024-12-11 23:50:39 -0500

    license file and copyright statement have been added

--- zigzagencodedriver.go
+++ zigzagencodedriver.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2024 ALTAI Consulting, Inc and Aleksey Gershgorin. All rights reserved.
+// Use of this source code is governed by MIT license that can be found in the LICENSE file.
+
+// Code demonstrates how to use encoding and decoding functions from altaiconsulting/zigzagencode package
 package main
 
 import (
